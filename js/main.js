d1fae564b5f5a88556fde335ada2c2e7cb00df5e
dthorpeuk <162460364+dthorpeuk@users.noreply.github.com>
2024-05-17 13:30:06 +0100

    Cookies fixes

--- js/main.js
+++ js/main.js
@@ -134,11 +134,9 @@ let elementConsent = document.querySelector(".manage-consent");
 let elementDialog = document.querySelector(".cookie-dialog-background");
 let elementAccept = document.querySelector(".dialog-row div + div");
 elementConsent.addEventListener('click',()=>{
-   // if(elementDialog.classList.contains("dialog-visible")){
-       // elementDialog.classList.remove("dialog-visible");
-   // }else{
+ 
         elementDialog.classList.add("dialog-visible");
-    //}
+  
 
 });
 elementAccept.addEventListener('click',()=>{
